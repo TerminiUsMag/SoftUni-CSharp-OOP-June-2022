6544f27fd487b445da64de7d3f36200d86b8144a
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-05 12:12:02 +0300

    04. Random List All done 100/100 :)

---
+++ Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var rand = new Random();
+            var randomIndex = rand.Next(0, base.Count - 1);
+            var removed = this[randomIndex];
+            this.RemoveAt(randomIndex);
+            return removed;
+        }
+    }
+}
---
+++ Inheritance - Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var customList = new RandomList();
+            customList.Add("Nikolai");
+            customList.Add("Ivan");
+            customList.Add("Krasi");
+            customList.Add("Aleks");
+            customList.Add("Genadi");
+            customList.Add("Dani");
+
+            Console.WriteLine(customList.RandomString());
+
+        }
+    }
+}

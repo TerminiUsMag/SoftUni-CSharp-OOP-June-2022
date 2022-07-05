f24e260481e4cab19fdd5a5143368ea1e2da0c62
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-05 11:53:15 +0300

    01.Single Inheritance All done 100/100 :)

---
+++ Inheritance - Lab/Farm/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var newAnimal = new Animal();
+            var newDog = new Dog();
+
+            newAnimal.Eat();
+            newDog.Bark();
+            newDog.Eat();
+            //newAnimal.Bark();
+        }
+    }
+}

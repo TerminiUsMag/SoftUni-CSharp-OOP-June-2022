ca1a7e10759fd9b2f08605adb7279e4603d667f9
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-08 19:34:27 +0300

    02. Cars all Done 100/100 :)\

---
+++ Interfaces and Abstraction - Lab/02. Cars/Seat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Seat {this.Model}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Color = color;
+            this.Model = model;
+            this.Battery = battery;
+        }
+
+        public int Battery { get; private set; }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}

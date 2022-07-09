e57eb5c4fd45a5853c6eb5d3d6b5618f3f596bad
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-09 20:34:07 +0300

    05. Restaurant all done 100/100 :)

---
+++ Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name,double caffeine) : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.5M;
+        public double Caffeine { get; set; }
+
+    }
+}

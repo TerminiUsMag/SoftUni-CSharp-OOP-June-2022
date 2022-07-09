24dd233def8edb9f1477a1c64443bac9c27df169
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-09 08:01:00 +0300

    03. Players and Monsters all done 100/100 :)

--- Inheritance - Exercise/PlayersAndMonsters/StartUp.cs
+++ Inheritance - Exercise/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,13 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-
+            var bladeKnight = new BladeKnight("TerminiUsMag", 99);
+            System.Console.WriteLine(bladeKnight);
         }
     }
 }

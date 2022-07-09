db133aeaaf389d04a4be3c7dd096fe9c450a3379
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-09 21:44:22 +0300

    06. Animals all done 100/100 :)

---
+++ Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value != null)
+                    this.name = value;
+                else
+                    throw new Exception("Invalid input!");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Invalid input!");
+                else
+                    this.age = value;
+            }
+        }
+        public Gender Gender { get; set; }
+
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            if (gender == "Male")
+                this.Gender = Gender.Male;
+            else if (gender == "Female")
+                this.Gender = Gender.Female;
+            else
+            {
+                throw new Exception("Invalid input!");
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            return $@"{this.GetType().Name}
+{this.Name} {this.Age} {this.Gender}
+{this.ProduceSound()}";
+        }
+    }
+}
--- Inheritance - Exercise/Animals/StartUp.cs
+++ Inheritance - Exercise/Animals/StartUp.cs
@@ -1,10 +1,49 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            var beastList = new List<Animal>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Beast!")
+            {
+                var secondInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var name = secondInput[0];
+                var age = int.Parse(secondInput[1]);
+                var gender = secondInput[2];
+                Animal newAnimal;
+                switch (cmd)
+                {
+                    case "Cat":
+                        newAnimal = new Cat(name, age, gender);
+                        break;
+                    case "Dog":
+                        newAnimal = new Dog(name, age, gender);
+                        break;
+                    case "Frog":
+                        newAnimal = new Frog(name, age, gender);
+                        break;
+                    case "Kitten":
+                        newAnimal = new Kitten(name, age);
+                        break;
+                    case "Tomcat":
+                        newAnimal = new Tomcat(name, age);
+                        break;
+                    default:
+                        newAnimal = null;
+                        break;
+                }
+                if (newAnimal != null)
+                    beastList.Add(newAnimal);
+            }
+            foreach (var beast in beastList)
+            {
+                Console.WriteLine(beast);
+            }
         }
     }
 }

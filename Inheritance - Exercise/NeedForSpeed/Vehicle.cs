c54b57b2affa7b7faeee9a8c02269d30a86f5e11
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-09 19:59:26 +0300

    04. Need for Speed all done 100/100 :)

--- Inheritance - Exercise/NeedForSpeed/Vehicle.cs
+++ Inheritance - Exercise/NeedForSpeed/Vehicle.cs
@@ -6,11 +6,20 @@ namespace NeedForSpeed
 {
     public class Vehicle
     {
-        private double defaultFuelConsumption;
+        private const double DefaultFuelConsumption = 1.25;
         public Vehicle(int horsePower, double fuel)
         {
-            this.defaultFuelConsumption = 1.25;
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
         }
-        
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public virtual void Drive(double km)
+        {
+            this.Fuel -= km * FuelConsumption;
+        }
     }
 }

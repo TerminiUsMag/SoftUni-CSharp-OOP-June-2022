24f76161297bce24a60e0ae9a7ad001ea1b1ec43
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-09 07:44:34 +0300

    01.Person all done 100/100 :)

--- Inheritance - Exercise/Person/StartUp.cs
+++ Inheritance - Exercise/Person/StartUp.cs
@@ -1,9 +1,23 @@
-﻿namespace Person
+﻿using System;
+
+
+namespace Person
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            //var person = new Person("Nikolai", 20);
+            //System.Console.WriteLine(person);
+
+            //var child = new Child("Dani", 13);
+            //System.Console.WriteLine(child);
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
 
         }
     }

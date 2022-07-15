6e748f50893c861baf5a6fa102ab438e6247eac7
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-15 20:45:28 +0300

    03. Shapes all done 80/100 unknown test not passed..

---
+++ Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public double Radius
+        {
+            get { return this.radius; }
+            private set
+            {
+                if (value > 0)
+                    this.radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return (this.Radius * this.Radius) * Math.PI;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.Radius * 2) * Math.PI;
+        }
+        public override string Draw()
+        {
+            return "Circle";
+        }
+    }
+}
---
+++ Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+        public double Height
+        {
+            get { return this.height; }
+            private set
+            {
+                if (value > 0)
+                    this.height = value;
+            }
+        }
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                if (value > 0)
+                    this.width = value;
+            }
+        }
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.Height + this.Width) * 2;
+        }
+        public override string Draw()
+        {
+            return "Rectangle";
+        }
+    }
+}

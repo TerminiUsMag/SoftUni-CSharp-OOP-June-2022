8b43a8b868ac7b59a19c70f903b30932f9868faf
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-15 20:52:01 +0300

    01. Define an Interface IPerson all done 100/100 :)

---
+++ Interfaces and Abstraction - Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private int age;
+        private string name;
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
+}

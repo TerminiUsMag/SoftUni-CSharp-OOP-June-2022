415b11890ccd2eb10576f723645cdd791cb9f5ed
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-15 20:15:36 +0300

    02. Animals All done 100/100

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavoriteFood = favouriteFood;
+        }
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public string FavoriteFood
+        {
+            get { return favouriteFood; }
+            private set { favouriteFood = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.favouriteFood}";
+        }
+    }
+}

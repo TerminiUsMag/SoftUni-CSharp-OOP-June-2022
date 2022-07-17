b3c0dc2db90220cf308d12a5d181bf96dd88a563
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-17 07:07:30 +0300

    In the middle of 03. Telephony

---
+++ Interfaces and Abstraction - Exercise/Telephony/IWebAccecable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+     interface IWebAccecable
+    {
+        void Browse(string url);
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/Telephony/Smartphone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IWebAccecable
+    {
+        public void Browse(string url)
+        {
+            for (int i = 0; i < url.Length; i++)
+            {
+
+            }
+        }
+
+        public void Call()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

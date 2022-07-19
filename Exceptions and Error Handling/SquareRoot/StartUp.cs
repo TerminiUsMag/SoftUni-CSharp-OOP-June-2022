ad3edf4965e8af04e6932f4832a329fba9456abc
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-19 21:31:29 +0300

    01. Square Root all done 100/100

---
+++ Exceptions and Error Handling/SquareRoot/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                    throw new ArgumentException("Invalid number.");
+                Console.WriteLine(Math.Sqrt(n));
+
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        }
+    }
+}

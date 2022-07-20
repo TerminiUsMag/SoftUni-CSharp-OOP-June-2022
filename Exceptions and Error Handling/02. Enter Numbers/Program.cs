100196be4e9aff1703c209605645563a0c90ccdd
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-21 00:46:29 +0300

    02. Enter Numbers all done 100/100 :)

--- Exceptions and Error Handling/02. Enter Numbers/Program.cs
+++ Exceptions and Error Handling/02. Enter Numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02._Enter_Numbers
 {
@@ -6,7 +7,42 @@ namespace _02._Enter_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var arr = ReadNumber(1, 100);
+                Console.WriteLine(String.Join(", ", arr));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static int[] ReadNumber(int start, int end)
+        {
+            var arr = new List<int>();
+            var minValue = start;
+            var maxValue = end;
+            while (arr.Count < 10)
+            {
+                int currNum = 0;
+                try
+                {
+                    currNum = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException fex)
+                {
+                    Console.WriteLine("Invalid Number!");
+                    continue;
+                }
+                if (currNum > minValue && currNum < maxValue)
+                {
+                    arr.Add(currNum);
+                    minValue = currNum;
+                }
+                else
+                    Console.WriteLine($"Your number is not in range {minValue} - 100!");
+            }
+            return arr.ToArray();
         }
     }
 }

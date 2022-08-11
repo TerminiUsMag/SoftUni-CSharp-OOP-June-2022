368124c675244a64bd75047c085029dfaa2e3a87
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-08-11 05:10:23 +0300

    C# OOP Exam - 09 April 2022 used for Exam Prep

---
+++ C# OOP Exam - 09 April 2022/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,32 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private readonly Dictionary<string, IPilot> pilots;
+        public PilotRepository()
+        {
+            pilots = new Dictionary<string, IPilot>();
+        }
+        public IReadOnlyCollection<IPilot> Models => pilots.Values;
+
+        public void Add(IPilot model) => pilots.Add(model.FullName, model);
+
+        public IPilot FindByName(string name) => pilots.ContainsKey(name) ? pilots[name] : null;
+
+        public bool Remove(IPilot model)
+        {
+            if (pilots.ContainsKey(model.FullName))
+            {
+                pilots.Remove(model.FullName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
